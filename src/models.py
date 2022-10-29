6e141489566228c38d1dc04f6eef3c35583409ee
Bianca11810 <Bianca11810@gmail.com>
2022-10-29 15:44:03 +0000

    second push

--- src/models.py
+++ src/models.py
@@ -1,89 +1,114 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.ext.hybrid import hybrid_property
-from werkzeug.security import generate_password_hash, check_password_hash
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from eralchemy import render_er
 
-db = SQLAlchemy()
+Base = declarative_base()
 
-class User(db.Model):
-    __tablename__ ='User'
-    id = db.Column(db.Integer, primary_key = True, unique = True)
-    name = db.Column(db.String(256))
-    email = db.Column(db.String(256), unique = True)
-    user_name = db.Column(db.String(256))
-    password = db.Column(db.String(256))
-    #favorites = db.Column(db.String(256))
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "email": self.email,
-            "user_name": self.user_name,
-            #"favorites": self.favorites
-            # do not serialize the password, its a security breach
-        }
-class Favorites(db.Model):
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+class User(Base):
+    __tablename__ ='Users'
+    id = Column(Integer, primary_key = True, unique = True)
+    name = Column(String(256))
+    email = Column(String(256))
+    user_name = Column(String(256))
+    password = Column(String(256))
+
+class Favorites(Base):
     __tablename__ = 'Favorites'
-    id = db.Column(db.Integer, primary_key = True, unique = True)
-    user_id = db.Column(db.Integer, ForeignKey('User.id'))
-    fave_id = db.Column(db.Integer)
-    item_type = db.Column(db.String(256))
-    name = db.Column(db.String(256))
+    id = Column(Integer, primary_key = True, unique = True)
+    user_id = Column(Integer, ForeignKey('Users.id'))
+    fave_id = Column(Integer)
+    item_type = Column(String(256))
+    name = Column(String(256))
 
-class Planets(db.Model):
-    __tablename__ = 'Planets'
-    id = db.Column(db.Integer, primary_key = True, unique = True)
-    name = db.Column(db.String(256))
-    rotation_period = db.Column(db.Integer)
-    orbital_period = db.Column(db.Integer)
-    diameter = db.Column(db.Integer)
-    climate = db.Column(db.String(256))
-    gravity = db.Column(db.String(256))
-    terrain = db.Column(db.String(256))
-    surface_water = db.Column(db.Integer)
-    population = db.Column(db.Integer)
+
+class Character(Base):
+    __tablename__ = 'Character'
+    id = Column(Integer, primary_key=True, unique = True)
+    name = Column(String(256))
+    birth_year = Column(String(256))
+    eye_color = Column(String(256))
+    gender = Column(String(256))
+    hair_color = Column(String(256))
+    height = Column(String(256)) 
+    mass = Column(String(256))
+    skin_color = Column(String(256))
+    homeworld = Column(String(256))
+    
+    def to_dict(self):
+        return {}
+
+class Starships(Base):
+    __tablename__ = 'Starships'
+    id = Column(Integer, primary_key=True, unique = True)
+    name = Column(String(256))
+    model = Column(String(256)) 
+    starship_class = Column(String(256))
+    manufacturer = Column(String(256))
+    cost_in_credits = Column(String(256))
+    length = Column(String(256))
+    crew = Column(String(256))
+    passengers = Column(String(256))
+    max_atmosphering_speed = Column(String(256))
+    hyperdrive_rating = Column(String(256))
+    MGLT = Column(String(256))
+    cargo_capacity = Column(String(256))
 
     def to_dict(self):
         return {}
 
-class People(db.Model):
-    __tablename__ = 'People'
-    id = db.Column(db.Integer, primary_key = True, unique = True)
-    name = db.Column(db.String(256))
-    height = db.Column(db.Integer)
-    mass = db.Column(db.Integer)
-    hair_color = db.Column(db.String(256))
-    skin_color = db.Column(db.String(256))
-    eye_color = db.Column(db.String(256))
-    birth_year = db.Column(db.String(256))
-    gender = db.Column(db.String(256))
+class Vehicles(Base):
+    __tablename__ = "Vehicles"
+    id = Column(Integer, primary_key = True, unique = True)
+    name = Column(String(256))
+    model = Column(String(256))
+    vehicle_class = Column(String(256))
+    manufacturer = Column(String(256))
+    length = Column(String(256))
+    cost_in_credits = Column(String(256))
+    crew = Column(String(256))
+    passengers = Column(String(256))
+    max_atmosphering_speed = Column(String(256))
+    cargo_capacity = Column(String(256))
 
     def to_dict(self):
         return {}
 
-# class starships(db.Model):
-#      __tablename__ = 'starships'
-#     id = Column(Integer, primary_key = True, unique = True)
-#     name = Column(String(256)),
-# 	model = Column(String(256)),
-# 	manufacturer = Column(String(256)),
-# 	crew = Column(Integer),
-# 	passengers = Column(Integer),
-# 	cargo_capacity = Column(Integer),
-# 	consumables = Column(String(256)),
-# 	hyperdrive_rating = Column(Integer),
+class Planets(Base):
+    __tablename__ = "Planets"
+    id = Column(Integer, primary_key = True, unique = True)
+    name = Column(String(256))
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(String(256))
+    population = Column(Integer)
+    climate = Column(String(256))
+    terrain = Column(String(256))
+    surface_water = Column(String(256))
 
-#     def __repr__(self):
-#         return '<User %r>' % self.username
-
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "email": self.email,
-#             # do not serialize the password, its a security breach
-#         }
+    def to_dict(self):
+        return {}
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
